52c5f116eb2cf9492bb6341d38c5fde0f7b18776
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-12-08 19:10:37 +0000

    .,.

--- js/script.js
+++ js/script.js
@@ -10,14 +10,21 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-UNIT5-4/sw.js", {
-    scope: "/ICS2O-UNIT5-4/",
+  navigator.serviceWorker.register("/ICS2O-unit5-04-select/sw.js", {
+    scope: "/ICS2O-unit5-04-select/",
   })
 }
 
 /**
- * This function displays an alert.
+ * this function
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  const age = parseInt(document.getElementById("age").value)
+  const day = String(document.getElementById("day").value)
+
+  if (day == "thursday" || day == "tuesday" || (age > 12 && age < 21)) {
+    document.getElementById("answer").innerHTML = "You can get a discount!"
+  } else {
+    document.getElementById("answer").innerHTML = "You can't get a discount"
+  }
 }
